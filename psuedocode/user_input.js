f6e7a00e7d0e8fd54c4c1b4b3121a2fc29d44574
AllisonAnz <AllisonAnzalone@gmail.com>
2022-05-05 09:27:14 -0500

    Add User input

---
+++ psuedocode/user_input.js
@@ -0,0 +1,10 @@
+//Write pseudo code that tells a user that the number they entered is not a 5 or a 6.
+
+// READ users input 
+//  SET user input to isfive
+//    IF isFive is 5 
+//      PRINT "your number is five"
+//    ELSE IF isFive is 6 
+//      PRINT "your number is six"
+//    ELSE 
+//      PRINT "your number is not 5 or 6"
