51941c6658944c19be375835734bd2827b5fafbc
AllisonAnz <AllisonAnzalone@gmail.com>
2022-05-05 09:14:21 -0500

    read_two_nums algo add

---
+++ psuedocode/read_two_nums.js
@@ -0,0 +1,6 @@
+//  Write pseudo code that reads two numbers and multiplies them together and print out their product
+
+// READ num1 and num2
+//  SET multi to num1 * num2 
+//    PRINT product 
+// END
