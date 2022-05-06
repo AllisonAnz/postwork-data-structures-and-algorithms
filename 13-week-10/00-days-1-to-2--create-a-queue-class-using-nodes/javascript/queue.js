6810dc3037e3e47e826e902636fa3f59ba719cb6
AllisonAnz <AllisonAnzalone@gmail.com>
2022-05-05 20:03:00 -0500

    Add queue Algo

--- 13-week-10/00-days-1-to-2--create-a-queue-class-using-nodes/javascript/queue.js
+++ 13-week-10/00-days-1-to-2--create-a-queue-class-using-nodes/javascript/queue.js
@@ -14,33 +14,74 @@ class Queue {
   // ADD NODE TO BACK OF QUEUE
   // USE DATA TO CREATE A NEW NODE AND ADD IT TO THE QUEUE
   enqueue(data) {
+    const node = new Node(data);
 
+    if (!this.rear) {
+      this.front = node;
+      this.rear = node;
+      return;
+    }
+
+    this.rear.next = node;
+    this.rear = node;
   }
 
   // REMOVE NODE FROM FRONT OF QUEUE AND RETURN IT
   dequeue() {
+    if (!this.front) {
+      return this.front;
+    }
 
+    const oldFront = this.front;
+
+    this.front = oldFront.next;
+
+    if (!this.front || !this.front.next) {
+      this.rear = this.front;
+    }
+
+    return oldFront;
   }
 
   // RETURN NODE AT FRONT WITHOUT REMOVING IT
   peek() {
-
+    return this.front;
   }
 
   // RETURN TRUE IF QUEUE IS EMPTY, OTHERWISE FALSE
   isEmpty() {
-
+    return !this.front;
   }
 
   // RETURN NUMBER OF NODES IN QUEUE, E.G. 10
   size() {
+    let count = 0;
+    let node = this.front;
 
+    while (node) {
+      ++count;
+      node = node.next;
+    }
+
+    return count;
   }
 
   // RETURN INTEGER REPRESENTING HOW FAR TARGET IS FROM FRONT OF QUEUE
   // IF TARGET ISN'T IN QUEUE, RETURN -1
   search(target) {
+    let node = this.front;
+    let distance = 0;
 
+    while (node) {
+      if (node.data === target) {
+        return distance;
+      }
+
+      ++distance;
+      node = node.next;
+    }
+
+    return -1;
   }
 }
 
